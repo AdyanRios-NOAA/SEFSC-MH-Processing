70767d5df186f5f1e1e5dddd8a3a7f0cfdb60e51
AdyanRios-NOAA <87143630+AdyanRios-NOAA@users.noreply.github.com>
2021-12-01 09:11:50 -0500

    new location for processed files

--- code/main_MH_prep.R
+++ code/main_MH_prep.R
@@ -1,9 +1,11 @@
 # Main Management History Processing
 
 # Load packages
-install.packages("pacman")
+# install.packages("librarian")
+librarian::shelf(here, tidyverse, lubridate, googlesheets4, dotenv, ROracle, keyring)
 
-pacman::p_load(tidyverse, lubridate, googlesheets4, dotenv, ROracle, keyring)
+# SET WD RELATIVE TO PROJECT LOCATION
+here::i_am('code/main_MH_prep.R')
 
 # For accessing tables as Google sheets, need to authenticate email address
 gs4_auth()
@@ -17,27 +19,27 @@ end_timeseries =  as.Date("2020-12-31", "%Y-%m-%d")
   # To also better handle NA or null fields, the species data was transposed so that species_name_type indicates to oringal species field name
   # Name is the species aggregate or species group name or when common name is ALL
   # The MH dataset is also transposed to have a single common name field and no null values
-load('./MH_clean_spp_tables.RData')
+load(here('data/interim', 'MH_clean_spp_tables.RData'))
 
 
 # 1: Bug related cleaning (these changes will eventually be made in the database)
   # Dataframe result = mh_cleaned
-source('./MH_data_bugs.R')
+source(here('code', 'MH_data_bugs.R'))
 
 
 # 2: Sector and species related expansion (aggregates, groups, all species); retain orignal columns, but fills in common name always
   # Dataframe result = mh_expanded
-source('./MH_expansion.R')
+source(here('code', 'MH_expansion.R'))
 
 
 # 3: Pre-processing clean up (new zone names, renaming "adjusted", creating various new variables)
   # Dataframe result = mh_ready
-source('./MH_pre_processing.R')
+source(here('code', 'MH_pre_processing.R'))
 
 
 # 4: Sorting by cluster and fill in dates
   # Dataframe result = mh_sort (before final species expansion cleaning)
   # Dataframe result = mh_sort2 (remove expanded species that are no longer in the group and adjust start and end dates to reflect species added and removed dates)
-source('./MH_process.R')
+source(here('code', 'MH_process.R'))
 
 # 5: Post-processing by collection - grouping related mtypes (mtype = cluster)
