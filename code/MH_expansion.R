1c20150c807c7eed0275242704c29fa1856d8046
Sarina Atkinson <71387505+SarinaAtkinson-NOAA@users.noreply.github.com>
2022-01-24 11:31:55 -0500

    #7 break up species expansion into two dataframes

--- code/MH_expansion.R
+++ code/MH_expansion.R
@@ -20,10 +20,22 @@ mh_sect_expanded <- mh_cleaned %>%
 detailed_xref <- read_sheet("https://docs.google.com/spreadsheets/d/1PViPVtqkY3q1fWUFGZm1UyIIYrFxt-YDitEUGaHBjsg/edit#gid=1115852389") %>%
   select(-MANAGEMENT_CATEGORY)
 
-# Check species table -- 7 have duplicates still
+# Check species table -- 5 have duplicates still because they are species that were a part of an aggregate, removed, then reinstated
 sp_info_use2 <- sp_info_use %>%
   group_by(FMP, SPP_TYPE, SPP_NAME, COMMON_NAME_USE) %>%
   summarise(N = n())
+
+# Break up species table into two tables
+  # (1) Single record for species in aggregate or group
+  # (2) More than one record for species in aggregate or group
+sp_info_use_s <- sp_info_use %>%
+  left_join(sp_info_use2, by = c("FMP", "SPP_TYPE", "SPP_NAME", "COMMON_NAME_USE")) %>%
+  filter(N == 1) %>% 
+  select(-N)
+sp_info_use_m <- sp_info_use %>%
+  left_join(sp_info_use2, by = c("FMP", "SPP_TYPE", "SPP_NAME", "COMMON_NAME_USE")) %>%
+  filter(N == 2) %>% 
+  select(-N)
 
 # Prep data for species expansion
 mh_sect_expanded <- mh_sect_expanded %>%
@@ -39,12 +51,12 @@ mh_sect_expanded <- mh_sect_expanded %>%
   # Remove records where name is null 
   filter(!is.na(SPP_NAME))
 
-# OVER EXPAND MH on species only when detailed mtype
-mh_sp_expanded_y <- mh_sect_expanded %>%
+# OVER EXPAND MH on species for detailed mtypes where there is a single record for that species in species aggrgate/group table
+mh_sp_expanded_y1 <- mh_sect_expanded %>%
   # Filter to remove non-detailed records
   filter(DETAILED == 'YES') %>%
   # Join to species list table by species name type, name,  and FMP
-  full_join(., sp_info_use, by = c("FMP", "SPP_TYPE", "SPP_NAME")) %>%
+  full_join(., sp_info_use_s, by = c("FMP", "SPP_TYPE", "SPP_NAME")) %>%
   # Remove species group/aggregate name that do not appear in the database or FMP that are not in the data (i.e. HMS)
   filter(!is.na(REGULATION_ID)) %>%
   mutate(COMMON_NAME_USE = case_when(is.na(COMMON_NAME_USE) ~ SPP_NAME,
@@ -53,14 +65,48 @@ mh_sp_expanded_y <- mh_sect_expanded %>%
                                       TRUE ~ SPECIES_ITIS_USE)) %>%
   # Remove species when expanded if effective date > removed date
   # Remove species when expanded if ineffective date < added date
-  # filter(EFFECTIVE_DATE < REMOVED_SP_DATE | is.na(REMOVED_SP_DATE) | INEFFECTIVE_DATE > ADDED_SP_DATE) %>%
-  # # Impute ineffective date with the removed date for records where there is a removed date, but no ineffective date
-  # mutate(IMP_INEFFECTIVE = case_when(is.na(INEFFECTIVE_DATE) & !is.na(REMOVED_SP_DATE) ~ 1,
-  #                                    TRUE ~ 0),
-  #        INEFFECTIVE_DATE = case_when(is.na(INEFFECTIVE_DATE) & !is.na(REMOVED_SP_DATE) ~ REMOVED_SP_DATE,
-  #                                     TRUE ~ INEFFECTIVE_DATE)) %>%
+  filter(EFFECTIVE_DATE < REMOVED_SP_DATE | is.na(REMOVED_SP_DATE) | INEFFECTIVE_DATE > ADDED_SP_DATE) %>%
+  # Impute ineffective date with the removed date for records where there is a removed date, but no ineffective date
+  mutate(IMP_INEFFECTIVE = case_when(is.na(INEFFECTIVE_DATE) & !is.na(REMOVED_SP_DATE) ~ REMOVED_SP_DATE,
+                                     TRUE ~ INEFFECTIVE_DATE),
+         IMP_EFFECTIVE = NA) %>%
   # Remove species expansion date variables
   select(-c(SPECIES_ITIS, SCIENTIFIC_NAME))
+
+# OVER EXPAND MH on species for detailed mtypes where there are multiple records for that species in species aggrgate/group table
+mh_sp_expanded_y2 <- mh_sect_expanded %>%
+  # Filter to remove non-detailed records
+  filter(DETAILED == 'YES') %>%
+  # Join to species list table by species name type, name,  and FMP
+  full_join(., sp_info_use_m, by = c("FMP", "SPP_TYPE", "SPP_NAME")) %>%
+  # Remove species group/aggregate name that do not appear in the database or FMP that are not in the data (i.e. HMS)
+  filter(!is.na(REGULATION_ID), !is.na(ADDED_SP_DATE)) %>%
+  mutate(COMMON_NAME_USE = case_when(is.na(COMMON_NAME_USE) ~ SPP_NAME,
+                                     TRUE ~ COMMON_NAME_USE),
+         SPECIES_ITIS_USE = case_when(is.na(SPECIES_ITIS_USE) ~ as.character(SPECIES_ITIS),
+                                      TRUE ~ SPECIES_ITIS_USE)) %>%
+  # Remove species when expanded if effective date > removed date
+  # Remove species when expanded if ineffective date < added date
+  filter(EFFECTIVE_DATE < REMOVED_SP_DATE | is.na(REMOVED_SP_DATE) | INEFFECTIVE_DATE > ADDED_SP_DATE) %>%
+  # Rank duplicate records for same species
+  group_by(REGULATION_ID) %>%
+  mutate(rnk = order(REMOVED_SP_DATE)) %>%
+  ungroup() %>%
+  # Remove species when expanded if effective date > added date
+  mutate(del = case_when(EFFECTIVE_DATE > ADDED_SP_DATE & EFFECTIVE_DATE > REMOVED_SP_DATE ~ 1,
+                         rnk == 2 & INEFFECTIVE_DATE < ADDED_SP_DATE ~ 1,
+                         TRUE ~ 0)) %>%
+  filter(del == 0) %>%
+  # Impute effective date for the duplicate record so that EFFECTIVE_DATE = ADD_SP_DATE, adjusting effective date retains both records and indicates during the gap in timeline the species was not a part of that aggregate/group
+  mutate(IMP_EFFECTIVE = case_when(rnk == 2 ~ ADDED_SP_DATE,
+                                   TRUE ~ EFFECTIVE_DATE)) %>%
+  # Impute ineffective date with the removed date for records where there is a removed date, but no ineffective date
+  mutate(IMP_INEFFECTIVE = case_when(is.na(INEFFECTIVE_DATE) & !is.na(REMOVED_SP_DATE) ~ REMOVED_SP_DATE,
+                                     INEFFECTIVE_DATE > REMOVED_SP_DATE ~ REMOVED_SP_DATE,
+                                     TRUE ~ INEFFECTIVE_DATE)) %>%
+  # Remove species expansion date variables
+  select(-c(SPECIES_ITIS, SCIENTIFIC_NAME, rnk, del))
+
 
 # Dataframe of non-detailed records in long form to join with expanded dataframe
 mh_sp_expanded_n <- mh_sect_expanded %>%
@@ -68,12 +114,19 @@ mh_sp_expanded_n <- mh_sect_expanded %>%
   filter(DETAILED == 'NO') %>%
   # Get fields to match
   mutate(COMMON_NAME_USE = SPP_NAME,
-         SPECIES_ITIS_USE = as.character(SPECIES_ITIS)) %>%
+         SPECIES_ITIS_USE = as.character(SPECIES_ITIS),
+         IMP_EFFECTIVE = NA,
+         IMP_INEFFECTIVE = NA) %>%
   select(-SPECIES_ITIS)
   
-mh_sp_expanded <- mh_sp_expanded_y %>%
-  bind_rows(mh_sp_expanded_n)
+mh_sp_expanded <- mh_sp_expanded_y1 %>%
+  bind_rows(mh_sp_expanded_y2, mh_sp_expanded_n)
 
-
+# Check 
+chk <- mh_sp_expanded %>%
+  filter(COMMON_NAME_USE == 'GROUPER, GAG', MANAGEMENT_TYPE == 'BAG LIMIT') %>%
+  select(REGULATION_ID, SECTOR, O_COMMON_NAME, O_SPECIES_AGGREGATE, O_SPECIES_GROUP, MANAGEMENT_TYPE, SPP_TYPE, SPP_NAME, SPECIES_ITIS_USE, COMMON_NAME_USE, 
+         START_YEAR, EFFECTIVE_DATE, INEFFECTIVE_DATE, IMP_EFFECTIVE, IMP_INEFFECTIVE, ADDED_SP_DATE, REMOVED_SP_DATE) %>%
+  arrange(EFFECTIVE_DATE)
 
 
