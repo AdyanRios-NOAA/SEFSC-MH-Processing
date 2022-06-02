12f8c6be496b242234889ab4003ffde3102676d3
katgodwin <kmg.godwin@gmail.com>
2022-06-02 17:09:21 -0400

    further car investigation into closures and agg layering

--- code/test scripts/test_CAR_closures_and_aggs.R
+++ code/test scripts/test_CAR_closures_and_aggs.R
@@ -1,169 +1,160 @@
-##CAR CLOSURES AND AGGREGATES TESTS##
+##CAR CLOSURES AND AGGREGATES TESTS####
+
+## redo closure assessment with new data tables ####
+
 #veiw all mng types
-mh_sort %>% distinct(MANAGEMENT_TYPE_USE)
+mh_expanded %>% distinct(MANAGEMENT_TYPE_USE)
+
 
 #ISOLATING CAR FMP
-MH_CARsort <- mh_sort %>%
+MH_CARsort <- mh_expanded %>%
   filter(FMP == "REEF FISH FISHERY OF PUERTO RICO AND THE U.S. VIRGIN ISLANDS")
 
-#CAR aggregates and closures
-MH_CARsort %>% filter(SPP_TYPE %in% c("SPECIES_GROUP","SPECIES_AGGREGATE", "COMMON_NAME"),
-         MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "ALLOWABLE SPECIES", "POSSESSION LIMIT"))%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
-  distinct(ZONE)
-
-#sort closures by zone and species
-MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "ALLOWABLE SPECIES", 
-                                                 "POSSESSION LIMIT", "PERMIT MORATORIUM", "LIMITED SPECIES"))%>%
-  select(SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-         VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE, END_DATE, SECTOR_USE, SPP_NAME, SPP_TYPE )%>%
-  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))%>%
+#ALL CLOSURES
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"))%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         COMMON_NAME_USE) %>%
   distinct(CLUSTER)
-    
-View(MH_CARsort)
 
-test = filter(mh_sort, CLUSTER %in% c(713, 714, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991,
-                                      1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,  2001, 2002, 2003, 2004, 1440, 1441, 
-                                      2046, 2049, 2050, 2051, 2052, 2053, 2054, 2048, 2055, 2047, 800, 801, 
-                                      802, 803, 1021, 1022, 796, 795, 39, 40, 100, 101, 165))
+test = filter(MH_CARsort, CLUSTER %in% c(39, 40, 100, 101, 102, 103, 104, 105, 165, 371, 372, 713, 714, 727, 728, 765, 766, 767, 768, 769, 770, 771, 772, 795, 796, 800, 801, 802, 803, 807, 808, 809, 810, 1021, 1022, 1440, 1441, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2046, 2047, 2048, 2049, 2050, 2051, 2052, 2053, 2054, 2055))
 select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, ZONE, SPP_NAME) %>%
-  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
-write.csv(test, "CAR_closure_clusters.csv")
-
-View(test)
-
-##NOW FOR JUST AGG closures
-MH_CARsort %>% filter(SPP_TYPE %in% c("SPECIES_GROUP","SPECIES_AGGREGATE"),
-                      MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "ALLOWABLE SPECIES", "POSSESSION LIMIT"))%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, 
-         SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID)%>%
-  distinct(ZONE)
-
-view(MH_CARsort)
-
-#agg closures only impact puerto rico managmenet area, 
-#St. tomas/St. john managment area, and S.t croix managment area
-#ALL AGG CLOSURES ARE ONCE
-
-##AGG ALL MNG TYPES
-MH_CARsort %>% filter(SPP_TYPE %in% c("SPECIES_GROUP","SPECIES_AGGREGATE"))%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
-  distinct(MANAGEMENT_TYPE_USE)
-#aggs only impact fishing season, ACL, bag limit, and closure
-
-#how many closures for CAR
-MH_CARsort %>% filter(SPP_TYPE %in% c("SPECIES_GROUP","SPECIES_AGGREGATE", "COMMON_NAME"),
-                      MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "ALLOWABLE SPECIES", "POSSESSION LIMIT"))%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
-  distinct(CLUSTER)
-#51 distinct clusters
-#INCORRECT, IT KEEPS INCLUDING DEFINITION RECORDS?? MAYBE NOT INCORRECT HOLD ON
-
-##NOW JUST REC
-MH_CARsort %>% filter(SPP_TYPE %in% c("SPECIES_GROUP","SPECIES_AGGREGATE", "COMMON_NAME"),
-                      MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "ALLOWABLE SPECIES", "POSSESSION LIMIT"),
-                      SECTOR == "RECREATIONAL")%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
-  distinct(CLUSTER)
-#796, 2046, 2048, 2049, 2050, 2051
-test = filter(mh_sort, CLUSTER %in% c(796, 2046, 2048, 2049, 2050, 2051))
-select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, ZONE, SPP_NAME) %>%
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, COMMON_NAME_USE) %>%
   arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
 View(test)
-#ALL REC AGG CLOSURES FALL WITHIN PUERTO RICO MNGT AREA
-#1 SEASONAL CLOSURE, REMAINING ARE ONE TIME CLOSURES
-#ALL ARE FOR SPECIES GROUPS, NO SINGULAR SPECIES CLOSURES
+write.csv(test, paste0("./Output/CAR test output/CAR_ALL_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
 
-##NOW JUST COM
-MH_CARsort %>% filter(SPP_TYPE %in% c("SPECIES_GROUP","SPECIES_AGGREGATE", "COMMON_NAME"),
-                      MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "ALLOWABLE SPECIES", "POSSESSION LIMIT"),
-                      SECTOR == "COMMERCIAL")%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
+#SEASONAL Recreational closures
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"),
+                      SECTOR_USE == "RECREATIONAL",
+                      MANAGEMENT_STATUS_USE == "SEASONAL")%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         COMMON_NAME_USE) %>%
   distinct(CLUSTER)
-# 100, 101, 165, 795, 2047, 2052, 2053, 2054, 2055
-test = filter(mh_sort, CLUSTER %in% c(100, 101, 165, 795, 2047, 2052, 2053, 2054, 2055))
+
+test = filter(MH_CARsort, CLUSTER %in% c(713, 1440, 1981, 1983, 1985, 1987, 1989, 1991, 
+                                         1993, 1995, 1997, 1999, 2001, 2003))
 select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, ZONE, SPP_NAME) %>%
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, COMMON_NAME_USE) %>%
   arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
 View(test)
-#1 FISHING SEASON, REST ARE ONE TIME CLOSURES
-#SPECIES GROUPS AND "SPECIES ALL"
-#NO REOPENINGS
-
-##now look at closures for "all species"
-MH_CARsort %>% filter(SPP_NAME == "ALL",
-                      MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "ALLOWABLE SPECIES", "POSSESSION LIMIT"),
-                      SECTOR == "COMMERCIAL")%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
+write.csv(test, paste0("./Output/CAR test output/CAR_SR_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
+                       
+#ONCE Recreational closures
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"),
+                      SECTOR_USE == "RECREATIONAL",
+                      MANAGEMENT_STATUS_USE == "ONCE")%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         COMMON_NAME_USE) %>%
   distinct(CLUSTER)
-#100, 101, 165
-test = filter(mh_sort, CLUSTER %in% c(100, 101, 165))
+
+test = filter(MH_CARsort, CLUSTER %in% c(39, 102, 104, 371, 713, 727, 765, 767, 769, 771, 796, 
+                                         800, 802, 807, 809, 1021, 2046, 2048, 2049, 2050, 2051))
 select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME) %>%
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, COMMON_NAME_USE) %>%
   arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
 View(test)
-#VARIETY OF CLOSED ZONES
-#ALL ONE TIME CLOSURES
+write.csv(test, paste0("./Output/CAR test output/CAR_OR_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
 
-#AGG SPECIFIC CLOSURES?
-MH_CARsort %>% filter(SPP_TYPE == "SPECIES_AGGREGATE",
-                      MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", 
-                                                 "ALLOWABLE SPECIES", "POSSESSION LIMIT"))%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
+#ONCE Commercial closures
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"),
+                      SECTOR_USE == "COMMERCIAL",
+                      MANAGEMENT_STATUS_USE == "ONCE")%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         COMMON_NAME_USE) %>%
   distinct(CLUSTER)
-#NO OG AGGS IN CLOSURES
 
-##investigate fishery year real fast
-test <- mh_sort %>% filter(MANAGEMENT_TYPE_USE == "FISHING YEAR")%>%
-  select(CLUSTER,REGULATION_ID, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, FMP, SUBSECTOR_USE, 
-         ZONE, EFFECTIVE_DATE, START_DATE, START_DAY, START_MONTH, START_YEAR, CHANGE_DATE, END_DAY, END_MONTH,
-         END_YEAR, END_DATE, SECTOR_USE, SPP_NAME, SECTOR_ID)
-view(test)
-#INVESTIGATE REG ID 2092 AND 2103 (CLUSTERS 678 AND 698)
-test = filter(mh_sort, CLUSTER == 698)
+test = filter(MH_CARsort, CLUSTER %in% c(40, 100, 101, 103, 105, 165, 372, 714, 728, 766, 768, 770, 772, 795, 801, 803, 808, 810, 1022, 2047, 2052, 2053, 2054, 2055))
 select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME) %>%
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, COMMON_NAME_USE) %>%
   arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
 View(test)
-#fishing year march 1 - march 1 because its for march 1 thru end of feb
-test = filter(mh_sort, CLUSTER == 678)
-select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME) %>%
-  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
-View(test)
+write.csv(test, paste0("./Output/CAR test output/CAR_OC_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
 
-#2103 = BLACK SEA BASS IN SASG - no closure component for adjustment to fishing year
-test2 = filter(mh_sort, MANAGEMENT_TYPE_USE == "CLOSURE", 
-              FMP == "SNAPPER-GROUPER FISHERY OF THE SOUTH ATLANTIC REGION",
-              SECTOR_USE == "COMMERCIAL",
-              SPP_NAME %in% c("ALL", "BASS, BLACK SEA"))
-select(test2, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME) %>%
-  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
-View(test2)
-
-#CAR bag limits
-MH_CARsort %>% filter(MANAGEMENT_TYPE_USE == "BAG LIMIT")%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
+#SEASONAL Commercial closures
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"),
+                      SECTOR_USE == "COMMERCIAL",
+                      MANAGEMENT_STATUS_USE == "SEASONAL")%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         COMMON_NAME_USE) %>%
   distinct(CLUSTER)
-#BAG LIMITS ARE SIMPLE, ZERO ADJUSTMENTS
-test = filter(mh_sort, CLUSTER %in% c(1770, 1771, 2036, 2037))
-select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME) %>%
-  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
-View(test)
-#ALL BAG LIMITS ARE AGGREGATES, NO SIMPLE SPECIES
 
-#CAR SIZE LIMITS
-MH_CARsort %>% filter(MANAGEMENT_TYPE_USE == "MINIMUM SIZE LIMIT")%>%
-  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID) %>%
-  distinct(CLUSTER)
-#9 SPECIES, NO AGGS, SIMPLE
-test = filter(mh_sort, CLUSTER %in% c(1162, 1163, 1166, 1167, 1178, 1179, 1180, 1181, 1182, 
-                                      1183, 1184, 1185, 1186, 1187, 1188, 1189, 1190, 1191))
+test = filter(MH_CARsort, CLUSTER %in% c(714, 1441, 1982, 1984, 1986, 1988, 1990, 1992, 1994, 1996, 1998, 2000, 2002, 2004))
 select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
-       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME) %>%
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, COMMON_NAME_USE) %>%
   arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
 View(test)
+write.csv(test, paste0("./Output/CAR test output/CAR_SC_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
+
+##NOTES: full species expansion makes very large data set because most closures, esp. one time closures, 
+  # are for "ALL SPECIES". For analysis, DWH closures closures were removed and "all species" were condensed
+  # to one line for species group
+
+# rerun without species expansion ####
+
+MH_CARsort <- mh_dates %>%
+  filter(FMP == "REEF FISH FISHERY OF PUERTO RICO AND THE U.S. VIRGIN ISLANDS")
+
+#SEASONAL Commercial closures
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"),
+                      SECTOR_USE == "COMMERCIAL",
+                      MANAGEMENT_STATUS_USE == "SEASONAL")%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         SPP_NAME, SPP_TYPE) %>%
+  distinct(CLUSTER)
+
+test = filter(MH_CARsort, CLUSTER %in% c(714, 1441, 1982, 1984, 1986, 1988, 1990, 1992, 1994, 1996, 1998, 2000, 2002, 2004))
+select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, SPP_TYPE) %>%
+  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
+
+write.csv(test, paste0("./Output/CAR test output/CAR_SC_DATES_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
+
+#SEASONAL Recreational closures
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"),
+                      SECTOR_USE == "RECREATIONAL",
+                      MANAGEMENT_STATUS_USE == "SEASONAL")%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         SPP_NAME, SPP_TYPE) %>%
+  distinct(CLUSTER)
+
+test = filter(MH_CARsort, CLUSTER %in% c(713, 1440, 1981, 1983, 1985, 1987, 1989, 1991, 
+                                         1993, 1995, 1997, 1999, 2001, 2003))
+select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, SPP_TYPE) %>%
+  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
+
+write.csv(test, paste0("./Output/CAR test output/CAR_SR_DATES_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
+
+## WHY IS IT GIVING ME ONCE IN SEASONAL FILTER??? 
+
+#ONCE Commercial closures
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"),
+                      SECTOR_USE == "COMMERCIAL",
+                      MANAGEMENT_STATUS_USE == "ONCE")%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         SPP_NAME, SPP_TYPE) %>%
+  distinct(CLUSTER)
+
+test = filter(MH_CARsort, CLUSTER %in% c(40, 100, 101, 103, 105, 165, 372, 714, 728, 766, 768, 770, 772, 795, 801, 803, 808, 810, 1022, 2047, 2052, 2053, 2054, 2055))
+select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, SPP_TYPE) %>%
+  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
+View(test)
+write.csv(test, paste0("./Output/CAR test output/CAR_OC_DATES_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
+
+#ONCE Recreational closures
+MH_CARsort %>% filter(MANAGEMENT_TYPE_USE %in% c("CLOSURE", "FISHING SEASON", "FISHING YEAR", "PROHIBITED SPECIES"),
+                      SECTOR_USE == "RECREATIONAL",
+                      MANAGEMENT_STATUS_USE == "ONCE")%>%
+  select(CLUSTER, STATUS_TYPE, MANAGEMENT_TYPE_USE, MANAGEMENT_STATUS_USE, SECTOR, SUBSECTOR_USE, ZONE, SECTOR_ID, 
+         SPP_NAME, SPP_TYPE) %>%
+  distinct(CLUSTER)
+
+test = filter(MH_CARsort, CLUSTER %in% c(39, 102, 104, 371, 713, 727, 765, 767, 769, 771, 796, 
+                                         800, 802, 807, 809, 1021, 2046, 2048, 2049, 2050, 2051))
+select(test, SECTOR_ID, vol, page, CLUSTER, STATUS_TYPE, MANAGEMENT_STATUS_USE, MANAGEMENT_TYPE_USE, 
+       VALUE, diff ,diff_days, EFFECTIVE_DATE, START_DATE, CHANGE_DATE,   END_DATE, SECTOR_USE, SPP_NAME, SPP_TYPE) %>%
+  arrange(STATUS_TYPE, MANAGEMENT_STATUS_USE, desc(START_DATE), desc(vol), desc(page))
+View(test)
+write.csv(test, paste0("./Output/CAR test output/CAR_OR_DATES_test", format(Sys.Date(), "%d%b%Y"), ".csv"), row.names = FALSE)
+
