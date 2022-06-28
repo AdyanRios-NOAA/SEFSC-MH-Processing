73b72a92d0091a320b08c71b11582dcfa8bf165a
GaitlynMalone <gaitlynmalone@gmail.com>
2022-06-28 17:24:24 -0400

    Address Bug ID 6616 in the database and therefore remove it from the code.

--- code/MH_data_bugs.R
+++ code/MH_data_bugs.R
@@ -736,10 +736,6 @@ mh_cleaned <- mh_cleaned %>%
   mutate(MANAGEMENT_TYPE = case_when(REGULATION_ID == 1437 ~ 'TRIP LIMIT ADJUSTMENT',
                                      TRUE ~ MANAGEMENT_TYPE))
 
-#Bug ID 6616 - Start Year removed for Fishing Year record
-mh_cleaned <- mh_cleaned %>%
-  mutate(START_YEAR = case_when(REGULATION_ID != 11770 ~ START_YEAR))
-
 #Bug ID 6617 - Start Year removed for Fishing Year record
 mh_cleaned <- mh_cleaned %>%
   mutate(START_YEAR = case_when(REGULATION_ID != 11767 ~ START_YEAR))
