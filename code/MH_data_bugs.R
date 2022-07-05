d0fd41900a152b5fef250b8b191699eb6b8d7742
GaitlynMalone <gaitlynmalone@gmail.com>
2022-07-05 12:00:13 -0400

    Address Bug IDs 6978 and 6986 in the database and therefore remove them from the script.

--- code/MH_data_bugs.R
+++ code/MH_data_bugs.R
@@ -522,14 +522,6 @@ mh_cleaned <- mh_cleaned %>%
   mutate(MANAGEMENT_TYPE = case_when(REGULATION_ID == 792 ~ "MINIMUM SIZE LIMIT ADJUSTMENT",
                                      TRUE ~ MANAGEMENT_TYPE))
 
-#Bug ID -68 - Start time should be removed since FR Notice stated 2400 hours
-mh_cleaned <- mh_cleaned %>%
-  mutate(START_TIME = case_when(REGULATION_ID != 2597 ~ START_TIME))
-
-#Bug ID -69 - Start time should be removed since FR Notice stated 2400 hours
-mh_cleaned <- mh_cleaned %>%
-  mutate(START_TIME = case_when(REGULATION_ID != 2598 ~ START_TIME))
-
 #Bug ID -70 - Start time should be removed since FR Notice stated 2400 hours
 mh_cleaned <- mh_cleaned %>%
   mutate(START_TIME = case_when(REGULATION_ID != 407 ~ START_TIME))
